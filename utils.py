9215feed829f773e377f1c553813b4bc2f960dc4
Alec Gunny <Alec Gunny>
2016-11-12 09:42:35 -0800

    added notebook and broke up spectrogram plotting util'

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import aifc
 import numpy as np
+import matplotlib.pyplot as plt
 
 def load_file(fname):
     '''
@@ -10,3 +11,16 @@ def load_file(fname):
     bits = aiff.readframes(aiff.getnframes())
     aiff.close()
     return np.fromstring(bits, np.short).byteswap()
+
+
+def plot_spectrogram(frequencies, times, values):
+    fig, ax = plt.subplots()
+    axis_spectrogram(ax, (frequencies, times, values))
+    return fig
+
+def axis_spectrogram(ax, stuff):
+    frequencies, times, values = stuff
+    extent = [times.min(), times.max(), frequencies.min(), frequencies.max()]
+    ax.imshow(values, extent=extent, origin='lower', aspect='auto')
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Frequency (Scale)')
